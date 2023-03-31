af94c740b96e26b6226b82b97dfb4b2316601579
Sofia <zinarizinazina@gmail.com>
2023-03-31 00:58:18 -0700

    add print

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Hallow world")
+from traceback import print_list
+import modules.perimetr as perimeter
+p1 = perimeter.perimetr_func([1, 2, 5])
+print(p1)
+
+p2 = perimeter.perimetr_func([1, 2, 5, 7, 1])
+print(p2)
+
+print("hallo world")
+print_list
